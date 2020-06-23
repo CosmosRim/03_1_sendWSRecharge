37c1b2f349e2b988b574d90fd863e3f883ab0bf8
zhuyu <neverlandkyo@gmail.com>
2020-06-23 02:01:04 -0400

    add function of post WS, and run with goroutine. goroutine finish signal choose sync.waitGroup rather than channel.

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/BurntSushi/toml"
 	_ "github.com/mattn/go-oci8"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"runtime"
+	"strings"
+	"sync"
 )
 
 const cfgPath string = "./config/"
@@ -14,8 +19,8 @@ const cfgPath string = "./config/"
 type cfgInfo struct {
 	Db     oDbInfo `toml:"oDbInfo"`
 	SrcSql srcSql
+	WsURL  string
 }
-
 type oDbInfo struct {
 	User string `toml:"oDbUser"`
 	Pwd  string `toml:"oDbPwd"`
@@ -23,7 +28,6 @@ type oDbInfo struct {
 	Port int    `toml:"oDbPort"`
 	Sid  string `toml:"oDbSid"`
 }
-
 type srcSql struct {
 	Get string `toml:"getInfo"`
 }
@@ -44,18 +48,60 @@ func getDbData(dsn string, querySql string, m map[string]string) {
 	if err != nil {
 		panic(err)
 	}
+	if db == nil {
+		fmt.Println("db is nil")
+	}
 	defer db.Close()
 
 	rows, err := db.Query(querySql)
 	if err != nil {
 		panic(err)
 	}
+	defer rows.Close()
 
 	for rows.Next() {
 		var cai chargActInfo
 		rows.Scan(&cai.RUT, &cai.ActCode)
 		m[cai.ActCode] = cai.RUT
 	}
+}
+
+func ctSoap(rut string, actCode string) string {
+	soap := "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+	soap += "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://com.ztesoft.zsmart/xsd\">\n"
+	soap += "    <soapenv:Header>\n"
+	soap += "        <xsd:AuthHeader>\n"
+	soap += "            <Username>NY</Username>\n"
+	soap += "            <Password>smart</Password>\n"
+	soap += "            <!--Optional:-->\n"
+	soap += "            <ChannelCode>Devetel</ChannelCode>\n"
+	soap += "        </xsd:AuthHeader>\n"
+	soap += "    </soapenv:Header>\n"
+	soap += "    <soapenv:Body>\n"
+	soap += "        <xsd:PaymentRequest><TransactionSN>0115840151600006AABA75</TransactionSN>\n"
+	soap += "            <!--Optional:-->\n"
+	soap += "            <TransactionDesc xsi:nil=\"true\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"/>\n"
+	soap += "            <RUT>" + rut + "</RUT>\n"
+	soap += "            <AccountCode>" + actCode + "</AccountCode>\n"
+	soap += "            <!--Optional:-->\n"
+	soap += "            <ServiceNumber xsi:nil=\"true\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"/>\n"
+	soap += "            <PaymentType>1</PaymentType>\n"
+	soap += "            <!--Optional:-->\n"
+	soap += "            <FolioID>2188351001</FolioID>\n"
+	soap += "            <PaymentAmount>12580</PaymentAmount>\n"
+	soap += "            <CURRENCY>CLP</CURRENCY>\n"
+	soap += "            <PostDate>2020-06-18T12:34:56</PostDate>\n"
+	soap += "            <PaymentChannelID>202-11</PaymentChannelID>\n"
+	soap += "            <PaymentMethod>99</PaymentMethod>\n"
+	soap += "            <PaymentGatewayChannel>DEVETEL</PaymentGatewayChannel>\n"
+	soap += "            <!--Optional:-->\n"
+	soap += "            <CreditCardType xsi:nil=\"true\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"/>\n"
+	soap += "            <!--Optional:-->\n"
+	soap += "            <BankCode xsi:nil=\"true\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"/>\n"
+	soap += "        </xsd:PaymentRequest>\n"
+	soap += "    </soapenv:Body>\n"
+	soap += "</soapenv:Envelope>\n"
+	return soap
 }
 
 func generateExcel(databaseUser string) {
@@ -82,7 +128,39 @@ func main() {
 
 	actInfo := make(map[string]string)
 	getDbData(dsn, cfg.SrcSql.Get, actInfo)
+
+	var wg sync.WaitGroup
 	for k, v := range actInfo {
-		fmt.Println(k, v)
+		wg.Add(1)
+		soapWS := ctSoap(v, k)
+		actId := k
+		go func() {
+			defer wg.Done()
+			res, err := http.Post(cfg.WsURL, "text/xml; charset=UTF-8", strings.NewReader(soapWS))
+			if err != nil {
+				fmt.Printf("[error] act: %s, http post err: %s\n", actId, err)
+				runtime.Goexit()
+			}
+
+			if res.StatusCode != http.StatusOK {
+				fmt.Printf("[error] act: %s, webService request failed, status is: %s\n", actId, res.StatusCode)
+				runtime.Goexit()
+			}
+
+			data, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				fmt.Printf("[error] act: %s, ioutil readAll err: %s\n", actId, err)
+				runtime.Goexit()
+			}
+
+			fmt.Printf("[succeed] act: %s, webService response: %s\n", actId, string(data))
+
+			res.Body.Close()
+			runtime.Goexit()
+		}()
 	}
+
+	wg.Wait()
+
+	fmt.Println("all finished.")
 }
